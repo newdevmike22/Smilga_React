46476633cc50dbbd6d392b62520596e8e16f57e7
MLDODSON <paola@dominicansoul.com>
2023-10-23 10:27:28 -0500

    Advanced React setup.
    Advanced React setup.

---
+++ advanced-react/src/App.jsx
@@ -0,0 +1,11 @@
+import ErrorExample from "./tutorial/01-useState/01-error-example";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h2><ErrorExample/></h2>
+    </div>
+  )
+}
+
+export default App
